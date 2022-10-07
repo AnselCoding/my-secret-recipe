ea432d685e9165e63f8eade7ccd6085684831c2c
Ansel90095 <anselsiao@gmail.com>
2022-10-07 23:17:21 +0800

    add Header component

---
+++ my-secret-recipe/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-sheet :height="sheetHeight">
+        <v-container class="fill-height black">
+            <v-row align="center" justify="center">
+                <h1 class="white--text font-italic font-weight-black text-center display-3 hidden-xs-only">
+                    My Secret Recipe
+                </h1>
+                <h1 class="white--text font-italic font-weight-black text-center display-1 hidden-sm-and-up">
+                    My Secret Recipe
+                </h1>
+            </v-row>
+        </v-container>
+    </v-sheet>
+</template>
+<script>
+export default {
+    name: 'Header',
+    data: () => ({
+        //
+    }),
+    computed: {
+        sheetHeight() {
+            switch (this.$vuetify.breakpoint.name) {
+                case 'xs': return '200px'
+                case 'sm': return '300px'
+                case 'md': return '300px'
+                case 'lg': return '300px'
+                case 'xl': return '350px'
+            }
+            return '300px'
+        }
+    }
+}
+</script>
