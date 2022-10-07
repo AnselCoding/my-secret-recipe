b5a59db65d31b58cf2d80b28fd144447668310dc
Ansel90095 <anselsiao@gmail.com>
2022-10-07 22:34:46 +0800

    TopNav to components

--- my-secret-recipe/src/App.vue
+++ my-secret-recipe/src/App.vue
@@ -1,62 +1,6 @@
 <template>
   <v-app>
-
-    <v-app-bar app color="primary" dark>
-      <!-- <div class="d-flex align-center">
-        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-
-        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
-      </div> -->
-      <!-- <div id="nav">
-        <router-link class="yellow--text" to="/">Home</router-link> |
-        <router-link class="white--text" to="/about">About</router-link>
-      </div> -->
-
-      <v-toolbar-tittle class="font-italic font-weight-black">My Secret Recipe</v-toolbar-tittle>
-
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn text>
-          Tools
-        </v-btn>
-        <v-btn text>
-          Food
-        </v-btn>
-        <v-btn text>
-          Recipes
-        </v-btn>
-        <!-- <v-btn to="/" text>
-          home
-        </v-btn>
-        <v-btn to="/about" text>
-          about
-        </v-btn> -->
-
-      </v-toolbar-items>
-      <v-spacer></v-spacer>
-      <span class="hidden-sm-and-up">
-        <v-btn @click.stop="drawer = !drawer" text>Menu</v-btn>
-      </span>
-
-      <!-- <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary right>
-      <v-list>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          >
-          {{item.title}}
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <TopNav />
     <v-main>
       <router-view />
     </v-main>
@@ -64,17 +8,14 @@
 </template>
 
 <script>
+import TopNav from '@/components/TopNav.vue'
 
 export default {
-  name: 'App',
-
+  name: "App",
+  components: {
+    TopNav
+  },
   data: () => ({
-    drawer: false,
-    items:[
-      { title:"Tools"},
-      { title:"Food"},
-      { title:"Recipes"},
-    ]
     //
   }),
 };
---
+++ my-secret-recipe/src/components/TopNav.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <v-app-bar app color="#575757" dark>
+            <v-toolbar-tittle class="font-italic font-weight-black">My Secret Recipe</v-toolbar-tittle>
+
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn v-for="item in items" :key="item.title" text>
+                    {{item.title}}
+
+                </v-btn>
+
+                <!-- <v-btn to="/" text>
+          home
+        </v-btn>
+        <v-btn to="/about" text>
+          about
+        </v-btn> -->
+
+            </v-toolbar-items>
+            <v-spacer></v-spacer>
+            <span class="hidden-sm-and-up">
+                <v-btn @click.stop="drawer = !drawer" text>Menu</v-btn>
+            </span>
+
+            <!-- <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary right>
+            <v-list>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    {{item.title}}
+                    <!-- <v-list-item-content>
+            <v-list-item-title></v-list-item-title>
+          </v-list-item-content> -->
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TopNav',
+
+    data: () => ({
+        drawer: false,
+        items: [
+            { title: "Tools" },
+            { title: "Food" },
+            { title: "Recipes" },
+        ]
+        //
+    }),
+};
+</script>
