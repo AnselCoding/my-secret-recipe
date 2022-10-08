803fd0ef0a30c7b34498d737727b1320f0cb8a48
Ansel90095 <anselsiao@gmail.com>
2022-10-08 17:54:14 +0800

    add data into store

--- my-secret-recipe/src/store/index.js
+++ my-secret-recipe/src/store/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import tools from './tools'
+import food from './food'
+import recipesM from './recipesM'
+import ingredients from './ingredients'
+import recipesD from './recipesD'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tools,
+    food,
+    recipesM,
+    ingredients,
+    recipesD
   },
   mutations: {
   },
