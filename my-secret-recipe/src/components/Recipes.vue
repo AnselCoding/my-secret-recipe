47237c964598d82f2c803ac4979cf32bcd0fe7f6
Ansel90095 <anselsiao@gmail.com>
2022-10-10 17:55:25 +0800

    update recipes layout

--- my-secret-recipe/src/components/Recipes.vue
+++ my-secret-recipe/src/components/Recipes.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container id="recipes">
+    <v-container class="containerBGC" id="recipes">
         <v-row>
             <v-col sm="10" offset-sm="1" md="8" offset-md="2">
                 <h3>食譜筆記 | Recipes</h3>
@@ -8,15 +8,28 @@
         <v-row>
             <v-col sm="10" offset-sm="1" md="8" offset-md="2">
                 <v-row>
-                    <v-col class="text-center" cols="12" sm="6">
-                        <span>It all starts from here</span>
+                    <v-col class="pr-0" cols="12" sm="6">
+                        <v-img aspect-ratio="3">
+                            <div class="recipesStartEnd">It all starts from here</div>
+                        </v-img>
+                        <v-sheet v-for="(recipe, i) in recipesOdd">
+                            <v-img aspect-ratio="1.5" :src="recipe.headPic">
+                                <div class="pl-5 pt-5 recipeTitle">{{recipe.name}}</div>
+                            </v-img>
+                        </v-sheet>
+                        <v-img v-show="isEndLeft" aspect-ratio="3">
+                            <div class="recipesStartEnd">End for now</div>
+                        </v-img>
                     </v-col>
-                    <v-col class="text-center" cols="12" sm="6" v-for="(recipe, i) in $store.state.recipesM">
-                        
-                        {{recipe.name}}
-                    </v-col>
-                    <v-col class="text-center" cols="12" sm="6">
-                        <span>End for now</span>
+                    <v-col class="pl-0" cols="12" sm="6">
+                        <v-sheet v-for="(recipe, i) in recipesEven">
+                            <v-img aspect-ratio="1.5" :src="recipe.headPic">
+                                <div class="pl-5 pt-5 recipeTitle">{{recipe.name}}</div>
+                            </v-img>
+                        </v-sheet>
+                        <v-img v-show="!isEndLeft" aspect-ratio="3">
+                            <div class="recipesStartEnd">End for now</div>
+                        </v-img>
                     </v-col>
                 </v-row>
             </v-col>
@@ -26,6 +39,45 @@
 <script>
 export default {
     name: 'Recipes',
-    data: () => ({}),
+    data: () => ({
+        //
+    }),
+    computed: {
+        recipesEven() {
+            var Even = [];
+            for (let index = 0; index < this.$store.state.recipesM.length; index = index + 2) {
+                Even.push(this.$store.state.recipesM[index]);
+            }
+            return Even
+        },
+        recipesOdd() {
+            var Odd = [];
+            for (let index = 1; index < this.$store.state.recipesM.length; index = index + 2) {
+                Odd.push(this.$store.state.recipesM[index]);
+            }
+            return Odd
+        },
+        isEndLeft() {
+            return this.recipesOdd < this.recipesEven
+        }
+    }
 }
 </script>
+
+<style>
+.recipeTitle {
+    color: white;
+    font-weight: bold;
+    -webkit-text-stroke: .5px black;
+}
+
+ .recipesStartEnd {
+    height: 100%;
+    background-color: #575757;
+    color: white;
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+    opacity: .5;
+}
+</style>
