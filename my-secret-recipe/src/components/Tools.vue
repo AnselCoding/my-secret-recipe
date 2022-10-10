2613e536568bbd66d3c8fb97c3bef14ef303282c
Ansel90095 <anselsiao@gmail.com>
2022-10-10 23:40:17 +0800

    add ToolsDialog

--- my-secret-recipe/src/components/Tools.vue
+++ my-secret-recipe/src/components/Tools.vue
@@ -10,29 +10,38 @@
                 <v-row>
                     <v-col class="pa-2" cols="12" sm="6" md="4" v-for="(tool, i) in $store.state.tools" :key="i">
                         <v-hover v-slot="{ hover }">
-                            <v-img aspect-ratio="2" :src="tool.pic">
-                                <v-expand-transition>
-                                    <div v-if="hover"
-                                        class="d-flex transition-fast-in-fast-out grey darken-2 v-img--reveal white--text"
-                                        >
-                                        {{tool.name}}
-                                    </div>
-                                </v-expand-transition>
-                            </v-img>
+                            <a @click.stop="dialog = true">
+                                <v-img aspect-ratio="2" :src="tool.pic">
+                                    <v-expand-transition>
+                                        <div v-if="hover"
+                                            class="d-flex transition-fast-in-fast-out grey darken-2 v-img--reveal white--text">
+                                            {{tool.name}}
+                                        </div>
+                                    </v-expand-transition>
+                                </v-img>
+                            </a>
                         </v-hover>
                     </v-col>
                 </v-row>
             </v-col>
         </v-row>
+        <ToolsDialog :dialog="dialog" :closeDialog="closeDialog"></ToolsDialog>
     </v-container>
 </template>
 
 <script>
+import ToolsDialog from './ToolsDialog.vue';
 export default {
-    name: 'Tools',
+    name: "Tools",
+    components: { ToolsDialog },
     data: () => ({
-        //
-    })
+        dialog: false,
+    }),
+    methods: {
+        closeDialog() {
+            this.dialog = false;
+        }
+    }
 };
 </script>
 
