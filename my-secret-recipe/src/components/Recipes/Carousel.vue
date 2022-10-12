ea8a919f6f42631f032f35e782c76ead09cb9b20
Ansel90095 <anselsiao@gmail.com>
2022-10-12 13:57:45 +0800

    update carousel picture position

--- my-secret-recipe/src/components/Recipes/Carousel.vue
+++ my-secret-recipe/src/components/Recipes/Carousel.vue
@@ -4,7 +4,7 @@
             <v-carousel-item v-for="(item,i) in $store.state.carousel" :key="i" class="">
                 <v-row :height="parallaxHeight" class="carouselItem">
                     <v-col cols="8" class="pa-0">
-                        <v-img :height="parallaxHeight" :src="item.pic"></v-img>
+                        <v-img position="center top" :height="parallaxHeight" :src="item.pic"></v-img>
                     </v-col>
                     <v-col cols="4" :height="parallaxHeight" class="carouselCard">
                         <a :href="item.url">
@@ -28,7 +28,7 @@ export default {
                 case 'xs': return '200'
                 case 'sm': return '300'
                 case 'md': return '300'
-                case 'lg': return '300'
+                case 'lg': return '350'
                 case 'xl': return '350'
             }
             return '300'
@@ -39,7 +39,7 @@ export default {
 <style>
 .carouselItem {
     position: relative;
-    top: 4%;
+    top: 3.4%;
 }
 
 .carouselCard {
